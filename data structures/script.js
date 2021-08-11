1ee6f0460276fe42da2599c8568286520ff52127
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-08-12 00:50:22 +0530

    object destructuring

--- data structures/script.js
+++ data structures/script.js
@@ -55,3 +55,79 @@
 // console.log(p, q, r); // 8,9,1
 
 //destructuring objects
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Blah blah",
+  categories: ["Italian", "Pizzeria", "vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Brushhetta", "Gralic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      //fri is an object inside an obj which is inside restaurant obj
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "bbsr odisha",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: "via del sole,21",
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName, // when want to call the variables a new name
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags); //exact same name as name, openingHours, categories
+
+//default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters); //[] (5) ["Focaccia", "Brushhetta", "Gralic", "Bread", "Caprese Salad"]
+// = [] in case it didnt exist, other wise it will give undefined
+
+//mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj); // wrap in parenthesis to mutate variables
+console.log(a, b); //23,7
+
+//nested objects
+// const { fri } = openingHours;
+// console.log(fri); {open: 11, close: 23}
+
+const {
+  fri: { open: o, close: c }, //name open and close as o and c
+} = openingHours;
+console.log(o, c); //11,23
