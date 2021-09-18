0ed7e2ea76b1ad706e85bd972863beaa83cfde9a
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-09-18 20:56:39 +0530

    codingchallenge2

--- Arrays/script.js
+++ Arrays/script.js
@@ -394,23 +394,26 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-/*
 const calcAverageHumanAge = function (ages) {
-  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-  const adults = humanAges.filter(age => age >= 18);
-  console.log(humanAges);
+  const humansAge = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humansAge.filter(age => age >= 18);
+  console.log(humansAge);
   console.log(adults);
-  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+
   const average = adults.reduce(
     (acc, age, i, arr) => acc + age / arr.length,
-    0
+    0,
   );
-  // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+
   return average;
 };
+
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+
+/*
+
 ///////////////////////////////////////
 // The Magic of Chaining Methods
 const eurToUsd = 1.1;
