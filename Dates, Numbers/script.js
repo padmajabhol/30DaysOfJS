e9c9c33e176b6086d87fa80c7c3f4991b87078c1
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2021-10-28 18:55:08 +0530

    asyn javascript

--- Dates, Numbers/script.js
+++ Dates, Numbers/script.js
@@ -4,12 +4,29 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+/////////////////////////////////////////////////
 // Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -17,24 +34,24 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+const accounts = [account1, account2];
 
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
+/////////////////////////////////////////////////
 // Elements
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
@@ -61,20 +78,24 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+/////////////////////////////////////////////////
+// Functions
+
 const displayMovements = function (movements, sort = false) {
-  containerMovements.innerHTML = ""; //removes the other payements in html
+  containerMovements.innerHTML = "";
 
   const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
+
     const html = `
-    <div class="movements__row">
-          <div class="movements__type movements__type--${type}">${
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}¢</div>
-        </div>
+        <div class="movements__value">${mov}€</div>
+      </div>
     `;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -83,69 +104,68 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}¢`;
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}¢`;
+  labelSumIn.textContent = `${incomes}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}¢`;
+  labelSumOut.textContent = `${Math.abs(out)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
+      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}`;
+  labelSumInterest.textContent = `${interest}€`;
 };
 
-const createUsernames = function (acc) {
-  acc.forEach(function (acc) {
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
     acc.username = acc.owner
       .toLowerCase()
-      .split(" ") //separate the string into words
-      .map(
-        name => name[0], //return first letter of each word
-      )
-      .join(""); //joins the first 3 letters
+      .split(" ")
+      .map(name => name[0])
+      .join("");
   });
 };
-
 createUsernames(accounts);
 
 const updateUI = function (acc) {
-  //Display movements
+  // Display movements
   displayMovements(acc.movements);
 
-  //display balance
+  // Display balance
   calcDisplayBalance(acc);
 
-  //display summary
+  // Display summary
   calcDisplaySummary(acc);
 };
 
-// Event handler
-
+///////////////////////////////////////
+// Event handlers
 let currentAccount;
 
 btnLogin.addEventListener("click", function (e) {
-  //prevent form from submitting
+  // Prevent form from submitting
   e.preventDefault();
+
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value,
   );
   console.log(currentAccount);
 
   if (currentAccount?.pin === Number(inputLoginPin.value)) {
-    //Display UI and welcome message
+    // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(" ")[0]
     }`;
@@ -153,9 +173,9 @@ btnLogin.addEventListener("click", function (e) {
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = "";
-    inputLoginPin.blur(); //field loses focus
+    inputLoginPin.blur();
 
-    //update UI
+    // Update UI
     updateUI(currentAccount);
   }
 });
@@ -163,21 +183,22 @@ btnLogin.addEventListener("click", function (e) {
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
-  const recieverAcc = accounts.find(
+  const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value,
   );
   inputTransferAmount.value = inputTransferTo.value = "";
 
   if (
     amount > 0 &&
-    recieverAcc &&
+    receiverAcc &&
     currentAccount.balance >= amount &&
-    recieverAcc?.username !== currentAccount.username
+    receiverAcc?.username !== currentAccount.username
   ) {
+    // Doing the transfer
     currentAccount.movements.push(-amount);
-    recieverAcc.movements.push(amount);
+    receiverAcc.movements.push(amount);
 
-    //updateUI
+    // Update UI
     updateUI(currentAccount);
   }
 });
@@ -188,10 +209,10 @@ btnLoan.addEventListener("click", function (e) {
   const amount = Number(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // add movement
+    // Add movement
     currentAccount.movements.push(amount);
 
-    //update ui
+    // Update UI
     updateUI(currentAccount);
   }
   inputLoanAmount.value = "";
@@ -208,12 +229,15 @@ btnClose.addEventListener("click", function (e) {
       acc => acc.username === currentAccount.username,
     );
     console.log(index);
-    //delete account
+    // .indexOf(23)
+
+    // Delete account
     accounts.splice(index, 1);
 
-    //hide ui
-    containerApp.style.opacity = 100;
+    // Hide UI
+    containerApp.style.opacity = 0;
   }
+
   inputCloseUsername.value = inputClosePin.value = "";
 });
 
@@ -227,120 +251,3 @@ btnSort.addEventListener("click", function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-// const eurToUsd = 1.1;
-
-// const movementsUSD = movements.map(mov => mov * eurToUsd);
-
-// console.log(movements);
-// console.log(movementsUSD);
-
-// const movementsDescriptions = movements.map((mov, i, arr) => {
-//   `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
-//     mov,
-//   )}}`;
-// });
-
-// const deposits = movements.filter(function (mov) {
-//   return mov > 0;
-// });
-// console.log(deposits);
-// const withdrawals = movements.filter(function (mov) {
-//   return mov < 0;
-// });
-// console.log(withdrawals);
-
-// const firstWithdrawal = movements.find(mov => mov < 0);
-
-// console.log(movements);
-// console.log(firstWithdrawal);
-
-// const account = accounts.find(acc => acc.owner === "Jessica Davis");
-// console.log(account);
-
-// console.log(movements.includes(-130)); //true
-
-// some CONDITION
-// console.log(movements.some(mov => mov === -130));
-
-// const anyDeposits = movements.some(mov => mov > 0);
-// console.log(anyDeposits);
-
-//every
-
-// console.log(movements.every(mov => mov > 0)); //false
-
-// separate callback
-// const deposit = mov => mov > 0;
-// console.log(movements.some(deposit));
-// console.log(movements.every(deposit));
-// console.log(movements.filter(deposit));
-
-//flat
-
-// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
-// console.log(arrDeep.flat(2)); //get full array,  second level of nesting
-
-// const overallBalance = accounts
-//   .map(acc => acc.movements)
-//   .flat()
-//   .reduce((acc, mov) => acc + mov, 0);
-// console.log(overallBalance);
-
-// // flatMap
-
-// const overallBalance2 = accounts
-//   .flatMap(acc => acc.movements)
-//   .reduce((acc, mov) => acc + mov, 0);
-// console.log(overallBalance2);
-
-// Sorting arrays
-
-// //strings
-// const owners = ["Jonas", "Zach", "Adam", "Martha"];
-// console.log(owners.sort()); //sorts by alphabetical order
-// console.log(owners); //mutates the original array
-
-// //Numbers
-// console.log(movements);
-
-// // return < 0, A, B
-// //return > 0, A, B
-// // movements.sort((a, b) => {
-// //   if (a > b) return 1;
-// //   if (b > a) return -1;
-// // });
-
-// movements.sort((a, b) => a - b);
-// console.log(movements);
-
-//creating and filling arrays
-
-// console.log([1, 2, 3, 4, 5, 6, 7]);
-// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
-
-// const x = new Array(7);
-// console.log(x); //empty array of 7 elememts
-
-// // console.log(x.map(() => 5)); //nothing happens
-
-// x.fill(1, 3, 5);
-// console.log(x); // [empty × 3, 1, 1, empty × 2]
-
-// //Array.from
-// const y = Array.from({ length: 7 }, () => 1);
-// console.log(y);
-
-// const z = Array.from({ length: 7 }, (_, i) => i + 1);
-
-// console.log(z);
-
-//practice
